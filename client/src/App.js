e142d3949a9d5330539c78205ad59883df1b28ef
Kevin Zhong <kevinzhongny@gmail.com>
2021-02-17 13:05:59 -0500

    created react client files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import Recipes from './Components/Recipes';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <Recipes />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Recipes.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Recipes() {
+    
+
+    return(
+        <div>
+            <h2>Hello World!</h2>
+        </div>
+    )
+}
+
+export default Recipes;
