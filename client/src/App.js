e9a382a40cc4102c32170f863ed0c6345cdc8b7b
Kevin Zhong <kevinzhongny@gmail.com>
2021-02-21 19:28:10 -0500

    created recipes component with read & delete functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import Recipes from './Components/Recipes';
+import Home from './Components/Home';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-        <Recipes />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App-header'>
+          <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/recipes">Recipes</Link>
+                </li>
+              </ul>
+            </nav>
+  
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/recipes">
+                <Recipes />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
