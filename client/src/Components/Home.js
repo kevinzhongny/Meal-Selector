127d90d4a3604bf15bb651107ffb7a49161f1d79
Kevin Zhong <kevinzhongny@gmail.com>
2021-02-21 23:22:54 -0500

    commit save before changing name to primary key

---
+++ client/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import Recipes from './Recipes';
+
+function Home() {
+    return(
+        <div>
+            Meal-Selector
+        </div>
+    )
+}
+
+export default Home;
--- client/src/Components/Recipes.js
+++ client/src/Components/Recipes.js
@@ -6,7 +6,11 @@ function Recipes() {
 
     async function getRecipes() {
         try {
-            const res = await axios.get('http://localhost:8080/recipes');
+            const res = await axios.get('http://localhost:8080/recipes', {
+                params: {
+                    name: "Mapo Tofu"
+                }
+            });
             setRecipes(res.data);
         } catch(e) {
             console.error(e, e.message);
@@ -17,24 +21,35 @@ function Recipes() {
         getRecipes();
     }, [])
 
+    async function deleteRecipe(recipeId) {
+        try {
+            const res = await axios.delete('http://localhost:8080/recipes/' + recipeId);
+            console.log(res.data);
+            getRecipes();
+        } catch (e) {
+            console.error(e, e.message);
+        }
+    }
+
     return(
         <div>
-            { recipes && recipes.map(recipe => <Recipe recipe= { recipe } />)}
+            { recipes && recipes.map(recipe => <Recipe recipe= { recipe } deleteRecipe = { deleteRecipe } key= { recipe.id }/>)}
         </div>
     )
 }
 
-function Recipe({ recipe }) {
+function Recipe({ recipe, deleteRecipe }) {
     return(
         <div>
-            <li key = { recipe.id }>
-                <h4>
-                    <ul>Name: { recipe.name }</ul>
-                    <ul>Cuisine: { recipe.cuisine }</ul>
-                    <ul>Ingredients: { recipe.ingredient1 }, { recipe.ingredient2 }, { recipe.ingredient3 }, { recipe.ingredient4 }, { recipe.ingredient5 }</ul>
-                    <ul><a href={ recipe.url }>{ recipe.url }</a></ul>
-                </h4>
-            </li>
+            <form>
+                <span>
+                    <ul><b>Name:</b> { recipe.name }<br></br>
+                    <b>Cuisine:</b> { recipe.cuisine }<br></br>
+                    <b>Ingredients:</b> { recipe.ingredient1 }, { recipe.ingredient2 }, { recipe.ingredient3 }, { recipe.ingredient4 }, { recipe.ingredient5 }<br></br>
+                    <a href={ recipe.url }>{ recipe.url }</a><br></br>
+                    <button onClick={ () => deleteRecipe(recipe.id) }>Delete Recipe</button></ul>
+                </span>
+            </form>
         </div>
     )
 }
